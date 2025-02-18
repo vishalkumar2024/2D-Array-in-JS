014316eb44f2ae0214a2524297bff1a39f9fba0e
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-02-18 16:23:33 +0530

    added one file

--- Set Matrix Zero/brute-force.js
+++ Set Matrix Zero/brute-force.js
@@ -1,19 +1,23 @@
+// Given an m x n integer matrix, this function returns the array such that
+// if an element is 0, set its entire row and column to 0's.
+
+
 function setMatrix(arr, m, n) {
     function setRow(i) {
         for (let j = 0; j < n; j++) {    //TC=O(n)
             if (arr[i][j] != 0) {
-                arr[i][j] = -1;
+                arr[i][j] = -Infinity;
             }
         }
     }
     function setCol(j) {       
-        for (let i = 0; i < m; i++) {   //TC=O(n)
+        for (let i = 0; i < m; i++) {   //TC=O(m)
             if (arr[i][j] != 0) {
-                arr[i][j] = -1;
+                arr[i][j] = -Infinity;
             }
         }
     }
-    for (let i = 0; i < m; i++) {       //TC=O(n*n)
+    for (let i = 0; i < m; i++) {       //TC=O(m*n)
         for (let j = 0; j < n; j++) {
             if (arr[i][j] == 0) {
                 setRow(i);
@@ -22,9 +26,9 @@ function setMatrix(arr, m, n) {
         }
     }
 
-    for (let i = 0; i < m; i++) {        //TC=O(n*n)
+    for (let i = 0; i < m; i++) {        //TC=O(m*n)
         for (let j = 0; j < n; j++) {
-            if (arr[i][j] == -1) {
+            if (arr[i][j] == -Infinity) {
                 arr[i][j] = 0;
             }
         }
@@ -34,3 +38,7 @@ function setMatrix(arr, m, n) {
 
 arr=[[1,1,1],[1,0,1],[1,1,1]];
 console.log(setMatrix(arr,3,3))
+
+// TC=O(n³)   O(m*n*(n+m)+(m*n))
+// SC=O(1)
+
