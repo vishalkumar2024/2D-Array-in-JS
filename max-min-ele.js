cb1fb3ca7482de434d40a8c7d5a03b898201a364
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-12-25 22:22:50 +0530

    little improved

--- max-min-ele.js
+++ max-min-ele.js
@@ -3,11 +3,11 @@ function MaxMin(arr,m,n){
     let min=Infinity;
     for(let i=0;i<m;i++){
         for(let j=0;j<n;j++){
-            if(arr[i][j]>max) max=arr[i][j]
-            if(arr[i][j]<min) min=arr[i][j]
+            if(arr[i][j]>max) max=arr[i][j];
+            if(arr[i][j]<min) min=arr[i][j];
         }
     }
-   return [min,max]
+   return [min,max];
 }
 
 let arr=[[11, 62, 83, 55], [18, 96, 17, 81], [95, 110, 101, 12]]
